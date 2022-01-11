e916cbf0c0e39770b3a84c702eab5dc937b8e2ba
abouterf <ekargosha88@gmail.com>
2022-01-11 14:52:23 +0330

    package publishes added

--- packages/abouterf/laravel-mobile-auth/src/ServiceProvider/LaravelMobileAuthServiceProvider.php
+++ packages/abouterf/laravel-mobile-auth/src/ServiceProvider/LaravelMobileAuthServiceProvider.php
@@ -26,5 +26,26 @@ class LaravelMobileAuthServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'LaravelMobileAuth');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->publishes([
+            __DIR__.'/../config/laravel-mobile-auth.php' => config_path('laravelMobileAuth.php')
+        ],'laravel-mobile-auth-config');
+
+
+        //lara docs/overriding package views(lara package development)
+
+        /*****
+        after publishing if u changed the main code u have to use '--force' to override the
+        published.
+
+        ****/
+
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/LaravelMobileAuth/')
+        ], 'laravel-mobile-auth-views');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations' => database_path('migrations')
+        ], 'laravel-mobile-auth-migrations');
     }
 }
